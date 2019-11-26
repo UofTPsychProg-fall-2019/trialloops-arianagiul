1ae7a58779af7f89d591a70af3868dc7d1264087
arianagiul <55919874+arianagiul@users.noreply.github.com>
2019-11-25 23:14:36 -0500

    Add files via upload
    final submission

--- Step1_MakeOneTrial.py
+++ Step1_MakeOneTrial.py
@@ -14,7 +14,7 @@ import pandas as pd
 import os, sys
 from psychopy import visual, core, event, gui, logging
 
-# open a white full screen window
+# open a white full screen window (making a second window for the beginning instructions)
 win = visual.Window(fullscr=True, allowGUI=False, color='white', unit='height') 
 
 # uncomment if you use a clock. Optional because we didn't cover timing this week, 
@@ -27,14 +27,31 @@ win = visual.Window(fullscr=True, allowGUI=False, color='white', unit='height')
 # sequence of events that happen on one trial
 # if you're stuck you can use the responseExercise.py answer as a starting point 
 
-# maybe start by making stimulus objects (e.g. myPic = visual.ImageStim(...))  
+#importing trial information
+conditions = pd.read_csv('conditions.csv')
 
-# then draw all stimuli
+# maybe start by making stimulus objects (e.g. myPic = visual.ImageStim(...))
+
+#trial prompt
+instruct_trialtop = visual.TextStim(win, pos=(0,0.75), height=None, color='black', text='Is this scene likely or unlikely?')
+#trial response keys
+instruct_trialbottomright = visual.TextStim(win, pos=(0.5,-0.75), height=None, color='black', text='Right Key = likely')
+instruct_trialbottomleft = visual.TextStim(win, pos=(-0.5,-0.75), height=None, color='black', text='Left Key = unlikely')
+#trial image stimulus
+yesno = visual.ImageStim(win, size=(1,1), pos=(0,0), image='pics/yes1.png')
+
+#draw all trial stimuli
+instruct_trialtop.draw()
+instruct_trialbottomright.draw()
+instruct_trialbottomleft.draw()
+yesno.draw()
 
 # then flip your window
+win.flip()
 
 # then record your responses
-
+leys = event.waitKeys(keyList=['right','left'])
+print(keys)
 
 #%% Required clean up
 # this cell will make sure that your window displays for a while and then 
--- Step2_LoopIt.py
+++ Step2_LoopIt.py
@@ -9,43 +9,101 @@ Use this template to turn Step 1 into a loop
 # this imports everything you might need and opens a full screen window
 # when you are developing your script you might want to make a smaller window 
 # so that you can still see your console 
+
 import numpy as np
 import pandas as pd
 import os, sys
+import ctypes
 from psychopy import visual, core, event, gui, logging
 
-# open a white full screen window
-win = visual.Window(fullscr=True, allowGUI=False, color='white', unit='height') 
+#experiment
+expName = 'WeirdScenes'
+fileName = ''
+while True:
+    expInfo = {'subjNum':''}
+    dlg = gui.DlgFromDict(dictionary=expInfo, title=expName)
+    if dlg.OK == False: core.quit()
+    fileName = expName + '_' + expInfo['subjNum']+'.csv'
+    break
 
-# uncomment if you use a clock. Optional because we didn't cover timing this week, 
-# but you can find examples in the tutorial code 
-#trialClock = core.Clock()
+#create data frame
+variables = ['trial', 'condition', 'scene', 'rt']
+outputs = pd.DataFrame(columns=variables)
 
+#create a window
+win = visual.Window(fullscr=True, allowGUI=False, color='white', unit='height')
+
+#unchanging information
+instruct_begin = visual.TextStim(win, pos=(0,0), height=None, color='black', alignHoriz='center', text="""You will be asked to decide whether or not a scene is likely or unlikely.
+
+Respond as quickly and as accurately as possible.
+
+If you think the scene is likely, press the right arrow key. If you think the scene is unlikely, press the left arrow key.
+
+Press the left or right arrow key to begin.""")
+instruct_trialtop = visual.TextStim(win, pos=(0,0.75), height=None, color='black', text='Is this scene likely or unlikely?')
+instruct_trialbottomright = visual.TextStim(win, pos=(0.5,-0.75), height=None, color='black', text='Right Key = likely')
+instruct_trialbottomleft = visual.TextStim(win, pos=(-0.5,-0.75), height=None, color='black', text='Left Key = unlikely')
+thanks = visual.TextStim(win, text='Thank you for your participation.', height=None, pos=(0,0), color='black')
+ntrials = 8
+
+#clock stuff
+trialClock = core.Clock()
+eventClock = core.Clock()
+
+#make a list or a pd.DataFrame that contains trial-specific info (stimulus, etc)
+conditions = pd.read_csv('conditions.csv')
+
+#randomizing
+conditions = conditions.sample(frac=1)
+conditions = conditions.reset_index()
 
 #%% your loop here
 # start by copying your one trial here, then identify what needs to be
 # changed on every trial.  Likely your stimuli, but you might want to change a few things
 
+#beginning instructions display until a key is pressed
+instruct_begin.draw()
+win.flip()
+event.waitKeys()
 
-# make a list or a pd.DataFrame that contains trial-specific info (stimulus, etc)
-# e.g. stim = ['1.jpg','2.jpg','3.jpg']
-
-
-# make your loop
-for t in ... :
+for t in np.arange(0, ntrials):
+    
+    event.clearEvents()
+    eventClock.reset()
     
     # include your trial code in your loop but replace anything that should 
     # change on each trial with a variable that uses your iterater
     # e.g. thisStimName = stim[t]
     #      thisStim = visual.ImageStim(win, image=thisStimName ...)
     
+    outputs.loc[t,'trial'] = t + 1
+    outputs.loc[t,'condition'] = conditions.loc[t,'condition']
+    outputs.loc[t,'scene'] = conditions.loc[t,'object']
+
+    yesno = visual.ImageStim(win, size=(1,1), pos=(0,0), image='pics/'+conditions.loc[t,'object'])
+    
+    #recording parameters
+    
+    trialClock.reset()
+    yesno.draw()
+    win.flip()
+    keys = event.waitkeys(maxWait = 4, keylist = ['left','right'])
+    
     # if you're recording responses, be sure to store your responses in a list
     # or DataFrame which also uses your iterater!
-
+    outputs.loc[t,'rt'] = trialClock.getTime()
+    
+    
 
 #%% Required clean up
 # this cell will make sure that your window displays for a while and then 
 # closes properly
+
+outputs.to_csv(fileName, index=False)
+
+thanks.draw()
+win.flip()
 
 core.wait(2)
 win.close()
